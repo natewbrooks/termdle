03da27bb191325ebda1aad8f670ef4540f2a781e
nate <59595919+natewbrooks@users.noreply.github.com>
2022-05-07 18:29:01 -0400

    finished

--- Termdle.java
+++ Termdle.java
@@ -8,8 +8,45 @@ public class Termdle {
 	public static ArrayList<String> answers = new ArrayList<String>();
 	public static ArrayList<String> acceptedWords = new ArrayList<String>();
 
+	public static HashMap<Character, Color> keyboardColors = new HashMap<Character, Color>() {{
+		put('q', Color.BLACK);
+		put('w', Color.BLACK);
+		put('e', Color.BLACK);
+		put('r', Color.BLACK);
+		put('t', Color.BLACK);
+		put('y', Color.BLACK);
+		put('u', Color.BLACK);
+		put('i', Color.BLACK);
+		put('o', Color.BLACK);
+		put('p', Color.BLACK);
+		put('a', Color.BLACK);
+		put('s', Color.BLACK);
+		put('d', Color.BLACK);
+		put('f', Color.BLACK);
+		put('g', Color.BLACK);
+		put('h', Color.BLACK);
+		put('j', Color.BLACK);
+		put('k', Color.BLACK);
+		put('l', Color.BLACK);
+		put('z', Color.BLACK);
+		put('x', Color.BLACK);
+		put('c', Color.BLACK);
+		put('v', Color.BLACK);
+		put('b', Color.BLACK);
+		put('n', Color.BLACK);
+		put('m', Color.BLACK);
+	}};
+
 	public static String[][] boardChars = new String[5][5];
 	public static Color[][] boardColors = new Color[5][5];
+	public static Panel[][] panels = new Panel[5][5];
+	public static Key[] keys = new Key[26];
+
+	public static Color correctLetterPlacementColor = Color.GREEN;
+	public static Color correctLetterColor = Color.ORANGE;
+	public static Color missedLetterColor = Color.DARK_GRAY;
+	public static Color notAWordColor = Color.RED;
+
 
 	public static String wordToGuess = "";
 	
@@ -17,6 +54,8 @@ public class Termdle {
 	public static int guessCount = 0;
 
 	public static boolean gameOver = false;
+	public static boolean errorWord = false;
+	public static int score = 0;
 	
 	public static UserInterface ui;
 	
@@ -37,6 +76,7 @@ public class Termdle {
 		for(int j = 0; j < boardColors.length; j++) {
 			for(int i = 0; i < boardColors[0].length; i++) {
 				boardColors[j][i] = Color.BLACK;
+				boardChars[j][i] = "";
 			}
 		}
 	}
@@ -46,17 +86,26 @@ public class Termdle {
 	}
 
 	public static void Check(KeyEvent e) {
+		if(errorWord && e.getKeyChar() == 8) {
+			for(int i = 0; i < 5; i++) {
+				Termdle.boardColors[Termdle.guessCount][i] = Color.BLACK;
+				Termdle.panels[guessCount][i].drawBox();
+			}
+			errorWord = false;
+		}
+		
 		//if the guess is not already 5 chars and the new char isn't special or a number
-		if(Termdle.guess.length() < 5 && Character.isLetter(e.getKeyChar())) {
-			Termdle.guess += e.getKeyChar();
+		char key = Character.toLowerCase(e.getKeyChar());
+		if(Termdle.guess.length() < 5 && Character.isLetter(key)) {
+			Termdle.guess += key;
 			// update ui with letter pressed
-			System.out.println(Termdle.guess);
+			ui.GetPanel().Type();
 		}
 
 		//if backspace is hit and the word has more than one letter, remove last letter
 		if(e.getKeyCode() == 8 && (Termdle.guess.length() > 0)) {
 			Termdle.guess = Termdle.guess.substring(0, Termdle.guess.length()-1);
-			System.out.println(Termdle.guess);
+			ui.GetPanel().Type();
 		}
 		
 		//if word length is 5 and they hit enter, check if word is in accepted list
@@ -64,18 +113,67 @@ public class Termdle {
 			if(Termdle.wordToGuess.equals(Termdle.guess)) {
 				// YOU WIN! WINNER WINNER!
 				Termdle.gameOver = true;
+				Termdle.score++;
+				ui.streakLabel.setText("YOU WON!! - R TO PLAY AGAIN");
+				ui.streakLabel.setForeground(correctLetterPlacementColor);
 				ui.GetPanel().checkCorrectLetters(Termdle.guess);
-				System.out.println("YOU WIN!!");
-				// update ui to make you happy
-			} else if(Termdle.acceptedWords.contains(Termdle.guess)) {
+				Termdle.UpdateKeyboard();
+ 				// update ui to make you happy
+			} else if(Termdle.acceptedWords.contains(Termdle.guess) && Termdle.guessCount == 4) {
+				ui.GetPanel().checkCorrectLetters(Termdle.guess);
+				ui.streakLabel.setText(Termdle.wordToGuess.toUpperCase() + " - R TO PLAY AGAIN");
+				ui.streakLabel.setForeground(Termdle.notAWordColor);
+				Termdle.gameOver = true;
+				Termdle.score = 0;
+				Termdle.UpdateKeyboard();
+			} else if (Termdle.acceptedWords.contains(Termdle.guess) ){
 				// update ui
 				ui.GetPanel().checkCorrectLetters(Termdle.guess);
 				Termdle.guess = "";
 				Termdle.NextLine();
+				Termdle.UpdateKeyboard();
 			} else {
 				// shake? popup saying not a word?
+				for(int i = 0; i < 5; i++) {
+					Termdle.boardColors[Termdle.guessCount][i] = Termdle.notAWordColor;
+					Termdle.panels[guessCount][i].drawBox();
+				}
+				errorWord = true;
 				System.out.println("That is not an accepted word!");
-			}			
+			}
+		}
+
+	}
+	public static void UpdateKeyboard() {
+		for (int i = 0; i < 26; i++) {
+			Termdle.keys[i].drawBox();
+		}
+	}
+
+	public static void RestartGame() {
+		boardChars = new String[5][5];
+		boardColors = new Color[5][5];
+		wordToGuess = "";
+		guess = "";
+		guessCount = 0;
+		gameOver = false;
+
+		ui.streakLabel.setForeground(Color.WHITE);
+		ui.streakLabel.setText("Score : " + score);
+		//select a random word from the list
+		int indexOfWord = (int) (Math.random() * answers.size());
+		wordToGuess = answers.get(indexOfWord);
+		System.out.println("Word to Guess: " + wordToGuess);
+		//wipe the word from the list so we can't reuse it
+		answers.remove(indexOfWord);
+
+		// set board to black
+		for(int j = 0; j < boardColors.length; j++) {
+			for(int i = 0; i < boardColors[0].length; i++) {
+				boardColors[j][i] = Color.BLACK;
+				boardChars[j][i] = "";
+				panels[j][i].drawBox();
+			}
 		}
 	}
 }
@@ -84,6 +182,8 @@ class MyKeyListener implements KeyListener {
 	public void keyPressed(KeyEvent e) {
 		if(!Termdle.gameOver) {
 			Termdle.Check(e);
+		} else if (Termdle.gameOver && Character.toUpperCase(e.getKeyCode()) == 82) {
+			Termdle.RestartGame();
 		}
 	}
 	
--- UserInterface.java
+++ UserInterface.java
@@ -5,6 +5,7 @@ import java.util.HashMap;
 public class UserInterface {
 
 	private Panel p = new Panel();
+	public JLabel streakLabel = new JLabel("Streak : " + Termdle.score);
 
 	public UserInterface() {
 		createGUI();
@@ -13,8 +14,88 @@ public class UserInterface {
 	public void createGUI() {
 		JFrame frame = new JFrame("Termdle");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setLayout(new GridBagLayout());
+		frame.getContentPane().setBackground(Color.BLACK);
+
+		JPanel board = new JPanel();
+		board.setLayout(new GridBagLayout());
+		board.setBackground(Color.BLACK);
+		board.setPreferredSize(new Dimension(400,300));
+
+		JPanel keyboard = new JPanel();
+		keyboard.setLayout(new GridBagLayout());
+		keyboard.setBackground(Color.BLACK);
+		keyboard.setPreferredSize(new Dimension(500,200));
 		
-		frame.add(p);
+		for(int h = 0; h < Termdle.keyboardColors.keySet().size(); h++) {
+			Key k = new Key();
+			GridBagConstraints c = new GridBagConstraints();
+			c.fill = GridBagConstraints.NONE;
+
+			if((double) Termdle.keyboardColors.keySet().size()/h > 2.0) {
+				c.gridx = h;
+				c.gridy = 3;
+			} else {
+				c.gridx = h-13;
+				c.gridy = 4;
+			}
+			
+			Termdle.keys[h] = k;
+
+			k.x.setText(Termdle.keyboardColors.keySet().toArray()[h].toString());
+			keyboard.add(k,c);
+		}
+
+		for(int j = 0; j < 5; j++) {
+			for(int i = 0; i < 5; i ++) {
+				Panel p = new Panel();
+				GridBagConstraints c = new GridBagConstraints();
+				c.fill = GridBagConstraints.NONE;
+				c.gridx = i+1;
+				c.gridy = j+1;
+
+				Termdle.panels[j][i] = p;
+				board.add(p,c);
+			}
+		}
+
+		JPanel text = new JPanel();
+		text.setBackground(Color.BLACK);
+
+		JPanel x = new JPanel();
+		x.setBackground(Color.BLACK);
+
+		JLabel label = new JLabel("Termdle");
+		label.setForeground(Color.WHITE);
+		label.setFont(new Font("Serif", Font.PLAIN, 25));
+
+		streakLabel.setForeground(Color.WHITE);
+		streakLabel.setFont(new Font("Serif", Font.PLAIN, 20));
+		
+		text.add(label);
+		x.add(streakLabel);
+		
+		GridBagConstraints cons = new GridBagConstraints();
+ 		cons.gridx = 5;
+		cons.gridy = 0;
+
+		frame.add(text, cons);
+		cons.gridx = 5;
+		cons.gridy = 1;
+		cons.ipady= 60;
+
+
+		frame.add(x, cons);
+		cons.gridx = 5;
+		cons.gridy = 2;
+
+
+		frame.add(board, cons);
+
+		cons.gridx = 5;
+		cons.gridy = 4;
+		cons.ipady= 30;
+		frame.add(keyboard, cons);
 		
 		frame.pack();
 		frame.setVisible(true);
@@ -26,33 +107,39 @@ public class UserInterface {
 }
 
 class Panel extends JPanel {
+	JLabel x = new JLabel("");
+
 	public Panel() {
-		setBorder(BorderFactory.createLineBorder(Color.BLACK));
+		setBorder(BorderFactory.createLineBorder(Color.WHITE));
+		setLayout(new FlowLayout());
+		x.setForeground(Color.WHITE);
+		x.setFont(new Font("Serif", Font.PLAIN, 30));
+		add(x,SwingConstants.CENTER);
+
 		this.addKeyListener(new MyKeyListener());
 		this.setFocusable(true);
 		this.requestFocusInWindow();
 	}
 	
 	public Dimension getPreferredSize() {
-		return new Dimension(500,800);
+		return new Dimension(60,60);
 	}
 	
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		drawBoxes(g);
+		drawBox();
 	}
-	
-	public void drawBoxes(Graphics g) {
+
+	public void drawBox() {
 		for(int j = 0; j < 5; j++) {
 			for(int i = 0; i < 5; i++) {
-				g.setColor(Termdle.boardColors[j][i]);
-				g.fillRect((i+1)*55,(j+1)*55,50,50);
-
-				// g.setColor(Color.WHITE);
-				// g.setFont(new Font("TimesRoman", Font.PLAIN, 35));
-				// g.drawString("Y", (i+1)*65, (j+1)*90);
+				if(Termdle.panels[j][i] == this) {
+					setBackground(Termdle.boardColors[j][i]);
+					x.setText(Termdle.boardChars[j][i]);
+				}
 			}
 		}
+		repaint();
 	}
 	
 	public void checkCorrectLetters(String guess) {
@@ -74,20 +161,66 @@ class Panel extends JPanel {
 			if(letter.equals(Termdle.wordToGuess.substring(i,i+1))) {
 				//set box to green
 				letterCount.put(c, letterCount.get(c)-1);
-				Termdle.boardColors[Termdle.guessCount][i] = Color.GREEN;
+				Termdle.boardColors[Termdle.guessCount][i] = Termdle.correctLetterPlacementColor;
+				Termdle.keyboardColors.put(c, Termdle.correctLetterPlacementColor);
 			} else if (Termdle.wordToGuess.contains(letter) && letterCount.get(c) > 0) {
 				// set box to orange
 				letterCount.put(c, letterCount.get(c)-1);
-				Termdle.boardColors[Termdle.guessCount][i] = Color.ORANGE;
-				
+				Termdle.boardColors[Termdle.guessCount][i] = Termdle.correctLetterColor;
+				if(Termdle.keyboardColors.get(c) != Termdle.correctLetterPlacementColor) {
+					Termdle.keyboardColors.put(c, Termdle.correctLetterColor);
+				}
 			} else {
-				Termdle.boardColors[Termdle.guessCount][i] = Color.BLACK;
+				Termdle.boardColors[Termdle.guessCount][i] = Termdle.missedLetterColor;
+				Termdle.keyboardColors.put(c, Termdle.missedLetterColor);
+				Termdle.keyboardColors.put(c, Termdle.missedLetterColor);
 			}
 		}
 		
 		this.repaint();
 	}
 
+	public void Type() {
+		for(int i = 0; i < 5; i++) {
+			try {
+				Termdle.boardChars[Termdle.guessCount][i] = Termdle.guess.substring(i, i+1);
+			} catch (Exception e) {
+				Termdle.boardChars[Termdle.guessCount][i] = "";
+			}
+		}
+
+		this.repaint();
+	}
+
 
 }
 
+class Key extends JPanel {
+	public JLabel x = new JLabel("");
+
+	public Key() {
+		setBorder(BorderFactory.createLineBorder(Color.WHITE));
+		setLayout(new FlowLayout());
+		x.setForeground(Color.WHITE);
+		x.setFont(new Font("Serif", Font.PLAIN, 15));
+		add(x,SwingConstants.CENTER);
+
+		this.addKeyListener(new MyKeyListener());
+		this.setFocusable(true);
+		this.requestFocusInWindow();
+	}
+	
+	public Dimension getPreferredSize() {
+		return new Dimension(30,30);
+	}
+	
+	public void paintComponent(Graphics g) {
+		super.paintComponent(g);
+		drawBox();
+	}
+
+	public void drawBox() {
+		setBackground(Termdle.keyboardColors.get(x.getText().toCharArray()[0]));
+		repaint();
+	}
+}
